2e956918e83dec4f1d5bb0a56cf3292f1b317264
neregist <animetrity@gmail.com>
2026-03-10 15:08:16 +0200

    init

--- src/main/java/com/github/animetrity/onlinepbx/OnlinePbxClient.java
+++ src/main/java/com/github/animetrity/onlinepbx/OnlinePbxClient.java
@@ -1,4 +1,43 @@
 package com.github.animetrity.onlinepbx;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.animetrity.onlinepbx.models.PbxUserResponse;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class OnlinePbxClient {
+    private final String domain;
+    private final String apiKey;
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    public OnlinePbxClient(String domain, String apiKey) {
+        this.domain = domain;
+        this.apiKey = apiKey;
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    private <T> T sendPostRequest(String endpoint, String body, Class<T> responseType) throws Exception {
+        // Формуємо правильний URL згідно з документацією: https://api.onlinepbx.ru/{domain}/...
+        String url = "https://api.onlinepbx.ru/" + this.domain + "/" + endpoint;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .header("x-pbx-authentication", this.apiKey) // Передаємо ключ у заголовку
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        return objectMapper.readValue(response.body(), responseType);
+    }
+
+    // Метод для отримання всіх користувачів
+    public PbxUserResponse getUsers() throws Exception {
+        return sendPostRequest("user/get.json", "", PbxUserResponse.class);
+    }
 }
--- src/main/java/com/github/animetrity/onlinepbx/models/PbxUserResponse.java
+++ src/main/java/com/github/animetrity/onlinepbx/models/PbxUserResponse.java
@@ -1,4 +1,32 @@
 package com.github.animetrity.onlinepbx.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PbxUserResponse {
+    private String status;
+
+    // ACCEPT_SINGLE_VALUE_AS_ARRAY гарантує, що ми завжди отримаємо List,
+    // навіть якщо API поверне лише одного користувача без квадратних дужок [].
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+    private List<User> data;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class User {
+        private int num;
+        private String name;
+        private String tr1;
+        private String tr2;
+        private String tr3;
+        private int delay1;
+        private int delay2;
+        private int delay3;
+        private boolean enabled;
+    }
 }
